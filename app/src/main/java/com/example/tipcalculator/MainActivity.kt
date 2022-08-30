ac8d60a845bbaeebc525108b7d8501df5aa68215
t-ryles <tajhakeem.ryles@gmail.com>
2022-08-30 09:03:19 -0500

    Refactored and tested code.

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -6,7 +6,7 @@ import com.example.tipcalculator.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,18 +16,25 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener { calculateTip() }
     }
 
-    fun calculateTip(){
+    private fun displayTip(tip: Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun calculateTip(){
         //Getting the user input cost of service
         val stringInTextField = binding.costOfService.text.toString()
 
         //Converting that input in a double for calculations
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
+            if (cost == null || cost == 0.00) {
+                displayTip(0.00)
+                return
+            }
 
         //Getting the ID of the checked radio button
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-
         //Setting the tip percentage for later calculations
-        val tipPercentage = when(selectedId) {
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> .20
             R.id.option_eighteen_percent -> .18
             else -> .15
@@ -37,14 +44,12 @@ class MainActivity : AppCompatActivity() {
         var tip = tipPercentage * cost
 
         //Getting the option to round up
-        val roundUp = binding.roundUpSwitch.isChecked
         //Rounding tip up is true
-        if (roundUp) {
+        if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
 
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+        displayTip(tip)
 
     }
 }
